80361d7186dc845a50c5ee6110d64902932ebcf9
Sagar <bhagwatsagar2290@gmail.com>
2023-10-21 13:27:28 +0530

    first commit

---
+++ 01basicreact/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h2>React with npx command</h2>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Chai from "./coffee"
+
+
+function App() {
+  const username = "Vite"
+
+  return (
+    <>
+    <Chai/>
+    <h1>react with {username}</h1>
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
