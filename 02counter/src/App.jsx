9646ad465d59317b026815b494083fa489bf268a
Sagar <bhagwatsagar2290@gmail.com>
2023-10-25 15:48:40 +0530

    props, tailwind and counter update

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,8 +9,21 @@ function App() {
   const addValue = () => {
     if (counter < 20) {
       setCounter(counter + 1);
+      // setCounter(counter + 1);  // This will not update by 4
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
     }
   };
+
+  // const addValueby4 = () => {
+  //   if (counter < 20) {
+  //     // This will add 4 in our counter
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }
+  // };
 
   const removeValue = () => {
     if (counter > 0) {
